6639155f4c54837063dbb9f91f1aac3d6c6bbb06
zafar2199 <75061841+zafar2199@users.noreply.github.com>
2021-01-19 12:38:43 +0300

    Add files via upload

---
+++ js/add.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('#adaptive').lightSlider({
+      adaptiveHeight:true,
+      auto:true,
+      item:1,
+      slideMargin:0,
+      loop:true
+  });
+});
+
+
+// gallery
+
+lightGallery(document.querySelector(".gallery"));
